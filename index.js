148f8f981e2d417c5ac416058d804c8d16802ae1
PJ Frias <peejfrias@gmail.com>
2017-11-03 22:23:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ function nowServing() {
   if (katzDeli.length === 0) {
     return "There is nobody waiting to be served!"
   } else {
-
+    return 'Currently serving ' + katzDeli[0] + '.'
+    katzDeli.shift()
   }
 }
 
