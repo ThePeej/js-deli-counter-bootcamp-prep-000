525bfb92e91c0369567ea40591aa262d7428c749
PJ Frias <peejfrias@gmail.com>
2017-11-03 21:59:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+var katzDeli = []
+
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(katzDeliLine.length+1 + ". " + name)
+}
+
+function currentline(katzDeliLine) {
+  var linenow = katzDeliLine
+  var n = linenow.length
+  if (n === 0) {
+    return "The line is currently empty."
+  } else {
+    var position = 1
+    while (n > 0) {
+      linenow[n] = position + ". " + linenow[n]
+      n--  
+    }
+  return linenow
+  }
+  
 }
